a637cf9cde515235e1cbab0fc0363c05f02fe116
Petter hassberg <dr.doddo@gmail.com>
2020-06-03 16:38:09 +0200

    Rearrange some stuff

--- setup.py
+++ setup.py
@@ -5,32 +5,30 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(name='octodns_lexicon',
-      version='0.1',
-      description='Lexicon provider for OctoDNS',
-      url='http://github.com/doddo/octodns-lexiconprovider',
-      author='Petter Hassberg',
-      author_email='dr.doddo@gmail.com',
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      license='MIT',
-      py_modules=["octodns_lexicon"],
-      classifiers=[
-            'Intended Audience :: Developers',
-            'Intended Audience :: System Administrators',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'Topic :: Internet :: Name Service (DNS)',
-            'Topic :: System :: Systems Administration',
-            'Topic :: Utilities',
-            "License :: OSI Approved :: MIT License",
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3',
-        ],
-      test_suite='nose.collector',
-      tests_require=['nose'],
-      install_requires=[
-            'octodns>=0.9.10',
-            'dns-lexicon>=3.3.23'
-          ]
-      )
-
+setuptools.setup(
+    name='octodns_lexicon',
+    version='0.1.dev1',
+    description='Lexicon provider for OctoDNS',
+    url='http://github.com/doddo/octodns-lexiconprovider',
+    author='Petter Hassberg',
+    author_email='dr.doddo@gmail.com',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    py_modules=["octodns_lexicon"],
+    classifiers=[
+         'Intended Audience :: Developers',
+         'Intended Audience :: System Administrators',
+         'Topic :: Software Development :: Libraries :: Python Modules',
+         'Topic :: Internet :: Name Service (DNS)',
+         'Topic :: System :: Systems Administration',
+         'Topic :: Utilities',
+         "License :: OSI Approved :: MIT License",
+         'Programming Language :: Python :: 2.7',
+         'Programming Language :: Python :: 3',
+    ],
+    tests_require=["pytest", "mock"],
+    install_requires=[
+        'octodns>=0.9.10',
+        'dns-lexicon>=3.3.23']
+)
